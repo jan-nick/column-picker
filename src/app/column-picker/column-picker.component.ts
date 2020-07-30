2234b11fa065cf05156759a7995090cb6a1e55e9
Jan Nicklas <jan.nicklas@tuta.io>
2020-07-30 19:33:10 +0200

    initial configs

--- src/app/column-picker/column-picker.component.ts
+++ src/app/column-picker/column-picker.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+enum Size {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large',
+}
+
 @Component({
   selector: 'column-picker',
   templateUrl: './column-picker.component.html',
-  styleUrls: ['./column-picker.component.scss']
+  styleUrls: ['./column-picker.component.scss'],
 })
 export class ColumnPickerComponent implements OnInit {
-  @Input() columns: number = 1;
+  @Input() columnData: [] | [][];
+  @Input() size: Size = Size.MEDIUM;
   @Input() cssClass: string;
 
-  constructor() { }
+  isMultiColumn = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.checkIfMultiColumn(this.columnData);
   }
 
+  checkIfMultiColumn(columnData: [] | [][]): void {
+    for (const el of columnData) {
+      if (el.constructor === Array) {
+        this.isMultiColumn = true;
+      }
+    }
+  }
 }
